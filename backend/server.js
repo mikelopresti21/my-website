9399a8878f5e8837625cc4ec3ae30542bf60e896
mikelopresti21 <mikelopresti21@gmail.com>
2025-03-05 12:03:00 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
+        pass: "jlrq upto xuzw ipmp"
     },
     tls: {
         rejectUnauthorized: false,
