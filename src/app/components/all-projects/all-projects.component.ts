dffd78625a7fa41500a73ac6e6edf258cbe887fb
mikelopresti21 <mikelopresti21@gmail.com>
2025-03-06 21:41:34 -0500

    reverting back

--- src/app/components/all-projects/all-projects.component.ts
+++ src/app/components/all-projects/all-projects.component.ts
@@ -12,13 +12,54 @@ import { ProjectsService } from '../../services/projects.service';
 })
 export class AllProjectsComponent {
   
-  projects: Project[] = [];
+  projects: Project[] = [
+    {
+      title: "Rust Away",
+      image: "/images/Rust_Away_Cover.jpg",
+      description: "A 2D platformer game created in UnityEngine.",
+      btnText: "Play Game",
+      route: "https://mikelo21.itch.io/rust-away",
+      internal: false
+    },
+    {
+      title: 'Simon',
+      image: '/images/simon-cover.png',
+      description: 'The classic Simon memory game, built with Html, CSS, and JavaScript.',
+      btnText: 'Play Game',
+      route: 'https://mikelopresti21.github.io/simon.html',
+      internal: false
+    },
+    {
+      title: 'Original Portfolio Website',
+      image: '/images/original-website-cover.jpg',
+      description: 'This website is a refactored version of my original portfolio site.',
+      btnText: 'Visit Website',
+      route: 'https://mikelopresti21.github.io/',
+      internal: false
+    },
+    {
+      title: 'Snake',
+      image: '/images/snake-cover.png',
+      description: 'The classic Snake game, built with HTML, CSS, and JavaScript.',
+      btnText: 'Play Game',
+      route: 'https://mikelopresti21.github.io/snake.html',
+      internal: false
+    },
+    {
+      title: 'WizOff',
+      image: '/images/WizOff_Cover.jpg',
+      description: 'A local two player video game made in Unity Engine.',
+      btnText: 'Play Game',
+      route: 'https://mikelo21.itch.io/wizoff',
+      internal: false
+    },
+  ];
 
-  constructor(private projectsService: ProjectsService) {}
+  // constructor(private projectsService: ProjectsService) {}
 
-  ngOnInit(){
-    this.projectsService.getProjects().subscribe((data) => {
-      this.projects = data;
-    });
-  }
+  // ngOnInit(){
+  //   this.projectsService.getProjects().subscribe((data) => {
+  //     this.projects = data;
+  //   });
+  // }
 }
