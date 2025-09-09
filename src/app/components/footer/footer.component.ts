7886ffb71aed2642eb2fd4b044b4aa54c3947402
mikelopresti21 <mikelopresti21@gmail.com>
2025-09-09 18:27:46 -0400

    contact page resizing, footer upated, all projects page small grid fixed

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,50 +12,5 @@ import { FooterLinks } from '../../footer-links';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  footerPages: FooterPages[] = [
-    {
-      title: 'Home',
-      route: '',
-    },
-    {
-      title: 'Projects',
-      route: 'projects',
-    },
-    {
-      title: 'Contact',
-      route: 'contact',
-    }
-  ]
 
-  footerGames: FooterGames[] = [
-    {
-      title: 'Rust Away',
-      link: 'https://mikelo21.itch.io/rust-away'
-    },
-    {
-      title: 'WizOff',
-      link: 'https://mikelo21.itch.io/wizoff'
-    },
-    {
-      title: 'Snake',
-      link: 'https://mikelopresti21.github.io/snake.html'
-    },
-    {
-      title: 'Simon',
-      link: 'https://mikelopresti21.github.io/simon.html'
-    }
-  ]
-
-  footerLinks: FooterLinks[] = [
-    {
-      image: 'github-icon.png',
-      title: 'GitHub',
-      link: 'https://github.com/mikelopresti21'
-    },
-    {
-      image: 'linkedin-icon.png',
-      title: 'LinkedIn',
-      link: 'https://www.linkedin.com/in/michaelvlopresti'
-    }
-  ]
 }
