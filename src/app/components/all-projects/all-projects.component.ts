d5dab8e9c0b9d55c18e87fe99de26040088295bd
mikelopresti21 <mikelopresti21@gmail.com>
2025-09-09 16:22:09 -0400

    resizing

--- src/app/components/all-projects/all-projects.component.ts
+++ src/app/components/all-projects/all-projects.component.ts
@@ -3,25 +3,28 @@ import { Project } from '../../project';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ProjectsService } from '../../services/projects.service';
+import { SpinnerComponent } from "../spinner/spinner.component";
 
 @Component({
   selector: 'app-all-projects',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, SpinnerComponent],
   templateUrl: './all-projects.component.html',
   styleUrl: './all-projects.component.css'
 })
 export class AllProjectsComponent implements OnInit{
 
   projects: Project[] = [];
+  isLoading = true;
 
   constructor(private projectsService: ProjectsService) {}
   
   ngOnInit(): void {
     this.projectsService.getProjects().subscribe((data) => {
+      this.isLoading = false;
       this.projects = data;
       this.projects.reverse();
     }, err => {
-      console.log("Failed to fetch projects");
+      this.isLoading = false;
     });
   }
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  showFullMenu: boolean = false;
+  private routerSub?: Subscription;
 
+  constructor (private router: Router){}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: UIEvent) {
+    if ((event.target as Window).innerWidth > 768) {
+      this.showFullMenu = false;
+    }
+  }
+
+  ngOnInit(): void {
+    // Subscribe to navigation events
+    this.routerSub = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.showFullMenu = false;
+      });
+  }
+
+  toggleFullMenu() {
+    this.showFullMenu = !this.showFullMenu;
+  }
 }
