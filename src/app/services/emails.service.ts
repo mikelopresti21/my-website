da2c592adede66e8009690c4f6ebf6de8f6b7e28
mikelopresti21 <mikelopresti21@gmail.com>
2025-09-16 08:44:14 -0400

    email service

--- src/app/services/emails.service.ts
+++ src/app/services/emails.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  private apiUrl = 'https://www.mikelopresti.com/contact';
+  private apiUrl = environment.apiUrl + 'contact';
 
   constructor(private http: HttpClient) { }
 
